a4c68de9c9306f15f7f415dbfa84984732cf0e48
Rawit Panjaroen <rawit.p@ku.th>
2020-09-09 02:53:29 +0700

    feat: add images delete command

--- cmd/delete.go
+++ cmd/delete.go
@@ -1,38 +1,28 @@
-/*
-Copyright © 2020 NAME HERE <EMAIL ADDRESS>
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
 package cmd
 
 import (
 	"fmt"
 
+	"github.com/senior-project-spai/spai-cli/service"
 	"github.com/spf13/cobra"
 )
 
 // deleteCmd represents the delete command
 var deleteCmd = &cobra.Command{
 	Use:   "delete",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
+	Short: "Delete all related image data from SPAI with the specific image ID",
+	Args:  cobra.ExactArgs(1),
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("delete called")
+	RunE: func(cmd *cobra.Command, args []string) error {
+		if err := service.RemoveImageFromDB(args[0]); err != nil {
+			return fmt.Errorf("deleteCmd: %w", err)
+		}
+
+		if err := service.RemoveImageFromS3(args[0]); err != nil {
+			return fmt.Errorf("deleteCmd: %w", err)
+		}
+
+		return nil
 	},
 }
 
